682df01c27cbad54746201073deee324dd16746c
Journey Cruz <journey.cruz@gmail.com>
2020-08-25 15:49:17 -0500

    change manifest.json to start on index.html

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require('path');
-const publicPath = path.join(__dirname, '..', 'public');
 
 // use Port provided by heroku or if none provided use Port 3000
 const PORT = process.env.PORT || 3000;
@@ -14,11 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-// serve index.html file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
