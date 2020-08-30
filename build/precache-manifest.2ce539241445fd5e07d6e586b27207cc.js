70b74e43a78ff7252fd08ab5c7efebfbcd165f40
Journey Cruz <journey.cruz@gmail.com>
2020-08-29 22:39:48 -0500

    create build folder

---
+++ build/precache-manifest.2ce539241445fd5e07d6e586b27207cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a2f988a3e6b054112692b5fe0d8ad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e70bdc6d5585b7cc75c0",
+    "url": "/static/css/main.9c0ba570.chunk.css"
+  },
+  {
+    "revision": "d8bf43f877d0b5589b24",
+    "url": "/static/js/2.7fbcb886.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.7fbcb886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70bdc6d5585b7cc75c0",
+    "url": "/static/js/main.dd06dec8.chunk.js"
+  },
+  {
+    "revision": "5b446e10af4fafd5a747",
+    "url": "/static/js/runtime-main.f71b05c2.js"
+  },
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/static/media/logo.26ef1cbc.svg"
+  }
+]);
